dfa1bb8011cfb7bd27833a3d85a714218d469d24
ThundaHorse <abeeeeekim@gmail.com>
2019-07-16 19:54:16 -0400

    modulating functions for refactor

--- Sweepstakes.php
+++ Sweepstakes.php
@@ -17,95 +17,95 @@
     <div class="container" style='background-color: powderblue;'>
       <br>
 
-    <?php
-      $firstNameErr = $lastNameErr = $emailErr = $phoneNumberErr = "";
-      $firstName = $lastName = $email = $phoneNumber = "";
+      <?php
+        $firstNameErr = $lastNameErr = $emailErr = $phoneNumberErr = "";
+        $firstName = $lastName = $email = $phoneNumber = "";
 
-      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        if (empty($_POST["firstName"])) {
-          $firstNameErr = "First Name is required";
-        } else {
-          $firstName = test_input($_POST["firstName"]);
-        }
-
-        if (empty($_POST["lastName"])) {
-          $lastNameErr = "Last name is required";
-        } else {
-          $lastName = test_input($_POST["lastName"]);
-        }
-
-        if (empty($_POST["email"])) {
-          $emailErr = "Email is required"; 
-        } else {
-          $email = test_input($_POST['email']);
-        
-          // check if e-mail address is well-formed
-          if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $emailErr = "Invalid email format"; 
-          }
-        }
-        
-        if (empty($_POST["phoneNumber"])) {
-          $phoneNumberErr = "Phone Number is required";
-        } else {
-          if (!preg_match("/^([0-9])+([\.|,]([0-9])*)?$/", $_POST['phoneNumber'])) {
-            $phoneNumberErr = 'Phone Number must be numerical';
+          if (empty($_POST["firstName"])) {
+            $firstNameErr = "First Name is required";
           } else {
-            $phoneNumber = test_input($_POST["phoneNumber"]);
+            $firstName = test_input($_POST["firstName"]);
+          }
+
+          if (empty($_POST["lastName"])) {
+            $lastNameErr = "Last name is required";
+          } else {
+            $lastName = test_input($_POST["lastName"]);
+          }
+
+          if (empty($_POST["email"])) {
+            $emailErr = "Email is required"; 
+          } else {
+            $email = test_input($_POST['email']);
+          
+            // check if e-mail address is well-formed
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+              $emailErr = "Invalid email format"; 
+            }
+          }
+          
+          if (empty($_POST["phoneNumber"])) {
+            $phoneNumberErr = "Phone Number is required";
+          } else {
+            if (!preg_match("/^([0-9])+([\.|,]([0-9])*)?$/", $_POST['phoneNumber'])) {
+              $phoneNumberErr = 'Phone Number must be numerical';
+            } else {
+              $phoneNumber = test_input($_POST["phoneNumber"]);
+            }
           }
         }
-      }
+          
+        function test_input($data) {
+          $data = trim($data);
+          $data = stripslashes($data);
+          $data = htmlspecialchars($data);
+          return $data;
+        }
         
-      function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-      }
+      ?>
+
+      <p><span class = "error">* required fields</span></p>
       
-    ?>
+        <form method = "post" action = "<?php 
+          echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+          <table>
+              <tr>
+                <td>First Name:</td>
+                <td><input type = "text" name = "firstName" placeholder='First Name'>
+                    <span class = "error">* <?php echo $firstNameErr;?></span>
+                </td>
+              </tr>
 
-    <p><span class = "error">* required fields</span></p>
-     
-      <form method = "post" action = "<?php 
-         echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-         <table>
-            <tr>
-               <td>First Name:</td>
-               <td><input type = "text" name = "firstName" placeholder='First Name'>
-                  <span class = "error">* <?php echo $firstNameErr;?></span>
-               </td>
-            </tr>
+              <tr>
+                <td>Last Name:</td>
+                <td><input type = "text" name = "lastName" placeholder='Last Name'>
+                    <span class = "error">* <?php echo $lastNameErr;?></span>
+                </td>
+              </tr>
+            
+              <tr>
+                <td>E-mail: </td>
+                <td><input type = "text" name = "email" placeholder='example@example.com'>
+                    <span class = "error">* <?php echo $emailErr;?></span>
+                </td>
+              </tr>
 
-            <tr>
-               <td>Last Name:</td>
-               <td><input type = "text" name = "lastName" placeholder='Last Name'>
-                  <span class = "error">* <?php echo $lastNameErr;?></span>
-               </td>
-            </tr>
-           
-            <tr>
-               <td>E-mail: </td>
-               <td><input type = "text" name = "email" placeholder='example@example.com'>
-                  <span class = "error">* <?php echo $emailErr;?></span>
-               </td>
-            </tr>
-
-            <tr>
-               <td>Phone Number: </td>
-               <td><input type = "text" name = "phoneNumber" placeholder='XXXXXXXXXX'>
-                  <span class = "error">* <?php echo $phoneNumberErr;?></span>
-               </td>
-            </tr>
-           
-            <td>
-               <button class='btn btn-info btn-md' type='submit' name='submit' value='submit'>
-                Submit
-               </button>
-            </td>
-         </table>
-      </form>
+              <tr>
+                <td>Phone Number: </td>
+                <td><input type = "text" name = "phoneNumber" placeholder='XXXXXXXXXX'>
+                    <span class = "error">* <?php echo $phoneNumberErr;?></span>
+                </td>
+              </tr>
+            
+              <td>
+                <button class='btn btn-info btn-md' type='submit' name='submit' value='submit'>
+                  Submit
+                </button>
+              </td>
+          </table>
+        </form>
 
       <?php 
         $dbPw = 'sh1balnom';
@@ -114,6 +114,7 @@
         $dbName = 'sweepstakes';
 
         $conn = new PDO('mysql:host='.$server.';dbname='.$dbName, $dbUsername, $dbPw);
+        
         $query = "INSERT INTO Sponsors (first_name, last_name, email, phone_number) VALUES ('$firstName', '$lastName', '$email', '$phoneNumber')";
 
         $exists = "SELECT * FROM Sponsors WHERE 'email' = '$email'";
@@ -133,21 +134,21 @@
       ?>
 
       <?php 
-         echo "<h2>
-                Your input
-              </h2>
-              <hr>";
-         echo 'First name: '.$firstName;
-         echo "<br>";
-         
-         echo 'Last name: '.$lastName;
-         echo "<br>";
-         
-         echo 'Phone Number: '.$phoneNumber;
-         echo "<br>";
-         
-         echo 'Email: '.$email;
-         echo "<br>";
+        echo "<h2>
+              Your input
+            </h2>
+            <hr>";
+        echo 'First name: '.$firstName;
+        echo "<br>";
+        
+        echo 'Last name: '.$lastName;
+        echo "<br>";
+        
+        echo 'Phone Number: '.$phoneNumber;
+        echo "<br>";
+        
+        echo 'Email: '.$email;
+        echo "<br>";
       ?>
 
     </div>
